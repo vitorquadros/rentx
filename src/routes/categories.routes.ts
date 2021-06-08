90f0d74ad79d607a2d6fe8c17f64f67361842183
Vitor Quadros <viitorquadros@hotmail.com>
2021-06-08 19:21:40 -0300

    feat: create uuid on create category route

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,19 @@
 import { Router } from 'express';
+import { v4 as uuid } from 'uuid';
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post('/categories', (req, res) => {
+categoriesRoutes.post('/', (req, res) => {
   const { name, description } = req.body;
 
-  categories.push({
+  const category = {
     name,
     description,
-  });
+    id: uuid(),
+  };
+  categories.push(category);
 
   return res.status(201).send();
 });
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,6 @@ import { categoriesRoutes } from './routes/categories.routes';
 const app = express();
 
 app.use(express.json());
-app.use(categoriesRoutes);
+app.use('/categories', categoriesRoutes);
 
 app.listen(3334, () => console.log('Running on http://localhost:3334'));
