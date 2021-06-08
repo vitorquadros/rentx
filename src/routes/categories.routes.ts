4b5f528bc51ca3f06337ff0542915d5671ba5f22
Vitor Quadros <viitorquadros@hotmail.com>
2021-06-08 19:25:22 -0300

    feat: create Category model

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,17 +1,19 @@
 import { Router } from 'express';
 import { v4 as uuid } from 'uuid';
+import { Category } from '../model/Category';
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post('/', (req, res) => {
   const { name, description } = req.body;
 
-  const category = {
+  const category: Category = {
     name,
     description,
     id: uuid(),
+    created_at: new Date(),
   };
   categories.push(category);
 
