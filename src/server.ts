4fcabde7f0075c6394e546dd76fe52cb35383791
Vitor Quadros <viitorquadros@hotmail.com>
2021-06-08 17:01:23 -0300

    feat: create categories route

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(categoriesRoutes);
+
+app.listen(3334, () => console.log('Running on http://localhost:3334'));
