2df37fbd2eb42becc9555159c836a578c7677414
Vitor Quadros <viitorquadros@hotmail.com>
2021-06-09 14:05:04 -0300

    feat: specifications route

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import { SpecificationRepository } from '../modules/cars/repositories/SpecificationRepository';
+import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService';
+
+export const specificationsRoutes = Router();
+const specificationsRepository = new SpecificationRepository();
+
+specificationsRoutes.post('/', (req, res) => {
+  const { name, description } = req.body;
+
+  const createSpecificationService = new CreateSpecificationService(
+    specificationsRepository
+  );
+
+  createSpecificationService.execute({ name, description });
+
+  return res.status(201).send();
+});
