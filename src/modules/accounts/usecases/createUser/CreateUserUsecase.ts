2b7075a11673a148c8fadf7685d372ee404ef722
Vitor Quadros <viitorquadros@hotmail.com>
2021-06-22 17:42:34 -0300

    feat: create CreateUserUsecase

---
+++ src/modules/accounts/usecases/createUser/CreateUserUsecase.ts
@@ -0,0 +1,27 @@
+import { inject, injectable } from 'tsyringe';
+import { ICreateUserDTO } from '../../DTOs/ICreateUserDTO';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+@injectable()
+export class CreateUserUsecase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({
+    driver_license,
+    email,
+    name,
+    password,
+    username,
+  }: ICreateUserDTO): Promise<void> {
+    await this.usersRepository.create({
+      driver_license,
+      email,
+      name,
+      password,
+      username,
+    });
+  }
+}
