bf7003572f39d91057a8fe92337424e122b93afc
Vitor Quadros <viitorquadros@hotmail.com>
2021-06-22 18:09:44 -0300

    feat: create CreateUserController

---
+++ src/modules/accounts/usecases/createUser/CreateUserController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import { CreateUserUsecase } from './CreateUserUsecase';
+
+export class CreateUserController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const { name, username, email, password, driver_license } = req.body;
+    const createUserUsecase = container.resolve(CreateUserUsecase);
+
+    await createUserUsecase.execute({
+      name,
+      username,
+      email,
+      password,
+      driver_license,
+    });
+
+    return res.status(201).send();
+  }
+}
