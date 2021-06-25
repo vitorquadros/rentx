7b3d73287301981f5321c92f963aa27b282d41c9
Vitor Quadros <viitorquadros@hotmail.com>
2021-06-25 14:21:30 -0300

    feat (dev): create CarsRepositoryInMemory

---
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -0,0 +1,35 @@
+import { ICreateCarDTO } from '@modules/cars/DTOs/ICreateCarDTO';
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
+import { ICarsRepository } from '../ICarsRepository';
+
+export class CarsRepositoryInMemory implements ICarsRepository {
+  cars: Car[] = [];
+
+  async create({
+    brand,
+    category_id,
+    daily_rate,
+    description,
+    fine_amount,
+    license_plate,
+    name,
+  }: ICreateCarDTO): Promise<void> {
+    const car = new Car();
+
+    Object.assign(car, {
+      brand,
+      category_id,
+      daily_rate,
+      description,
+      fine_amount,
+      license_plate,
+      name,
+    });
+
+    this.cars.push(car);
+  }
+
+  async findByLicensePlate(license_plate: string): Promise<Car> {
+    return this.cars.find((car) => car.license_plate === license_plate);
+  }
+}
