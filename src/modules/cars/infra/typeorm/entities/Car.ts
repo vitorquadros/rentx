afd2aaf1491c4d817fd6e9b1d850e1d864abcbc8
Vitor Quadros <viitorquadros@hotmail.com>
2021-06-27 15:45:13 -0300

    feat: implement typeorm on Car entity

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,23 +1,53 @@
-import { Entity } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from 'typeorm';
 import { v4 as uuid } from 'uuid';
+import { Category } from './Category';
 
+@Entity('cars')
 export class Car {
+  @PrimaryColumn()
   id: string;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @Column()
   daily_rate: number;
-  available: boolean;
+
+  @Column()
+  available = true;
+
+  @Column()
   license_plate: string;
+
+  @Column()
   fine_amount: number;
+
+  @Column()
   brand: string;
+
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @Column()
   category_id: string;
+
+  @CreateDateColumn()
   created_at: Date;
 
   constructor() {
     if (!this.id) {
       this.id = uuid();
-      this.available = true;
-      this.created_at = new Date();
     }
   }
 }
