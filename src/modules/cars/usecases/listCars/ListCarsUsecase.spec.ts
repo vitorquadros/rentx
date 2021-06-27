612d0bd2d0f59b6eb754f35b578359422ca3c486
Vitor Quadros <viitorquadros@hotmail.com>
2021-06-27 18:14:58 -0300

    test: ensure its able to list all available cars

---
+++ src/modules/cars/usecases/listCars/ListCarsUsecase.spec.ts
@@ -0,0 +1,28 @@
+import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
+import { ListCarsUsecase } from './ListCarsUsecase';
+
+let listCarsUsecase: ListCarsUsecase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+
+describe('List Cars', () => {
+  beforeEach(async () => {
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
+    listCarsUsecase = new ListCarsUsecase(carsRepositoryInMemory);
+  });
+
+  test('Should be able to list all available cars', async () => {
+    const car = await carsRepositoryInMemory.create({
+      brand: 'Car',
+      category_id: 'category_id',
+      daily_rate: 110.0,
+      description: 'Car Test',
+      fine_amount: 100,
+      license_plate: 'DEF-4140',
+      name: 'Car A-1',
+    });
+
+    const cars = await listCarsUsecase.execute();
+
+    expect(cars).toEqual([car]);
+  });
+});
