7df0a8ba80e6a1b1004b115c9f7509b8e1a65e0b
Vitor Quadros <viitorquadros@hotmail.com>
2021-06-28 17:41:40 -0300

    feat: create CarImage entity

---
+++ src/modules/cars/infra/typeorm/entities/CarImage.ts
@@ -0,0 +1,16 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('cars_image')
+export class CarImage {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  car_id: string;
+
+  @Column()
+  image_name: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+}
