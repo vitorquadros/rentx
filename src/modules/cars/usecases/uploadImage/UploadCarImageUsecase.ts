42d8dcb3e99f41122120cf3418c9e5d7fe75d69b
Vitor Quadros <viitorquadros@hotmail.com>
2021-06-28 17:53:43 -0300

    feat: create UploadCarImageUsecase

--- src/modules/cars/usecases/uploadImage/UploadCarImageUsecase.ts
+++ src/modules/cars/usecases/uploadImage/UploadCarImageUsecase.ts
@@ -1,1 +1,21 @@
-export class UploadCarImageUsecase {}
+import { inject, injectable } from 'tsyringe';
+import { ICarsImagesRepository } from '@modules/cars/repositories/ICarsImagesRepository';
+
+interface IRequest {
+  car_id: string;
+  images_name: string[];
+}
+
+@injectable()
+export class UploadCarImageUsecase {
+  constructor(
+    @inject('CarsImageRepository')
+    private carsImagesRepository: ICarsImagesRepository
+  ) {}
+
+  async execute({ car_id, images_name }: IRequest): Promise<void> {
+    images_name.map(async (image) => {
+      await this.carsImagesRepository.create(car_id, image);
+    });
+  }
+}
