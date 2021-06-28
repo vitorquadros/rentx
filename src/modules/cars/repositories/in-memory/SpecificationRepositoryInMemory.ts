61b02b594a645eb579f3bec499f7e695cd036e85
Vitor Quadros <viitorquadros@hotmail.com>
2021-06-28 13:29:43 -0300

    fix: interfaces and returns

--- src/modules/cars/repositories/in-memory/SpecificationRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/SpecificationRepositoryInMemory.ts
@@ -9,13 +9,17 @@ export class SpecificationRepositoryInMemory // eslint-disable-next-line indent
 {
   specifications: Specification[] = [];
 
-  async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
+  async create({
+    name,
+    description,
+  }: ICreateSpecificationDTO): Promise<Specification> {
     const specification = new Specification();
     Object.assign(specification, {
       description,
       name,
     });
     this.specifications.push(specification);
+    return specification;
   }
 
   async findByName(name: string): Promise<Specification> {
