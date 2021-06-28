6bc509984252217ba1b8105874194e5c14465745
Vitor Quadros <viitorquadros@hotmail.com>
2021-06-28 17:58:19 -0300

    feat: create UploadCarImageController

---
+++ src/modules/cars/usecases/uploadImage/UploadCarImageController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import { UploadCarImageUsecase } from './UploadCarImageUsecase';
+
+interface IFiles {
+  filename: string;
+}
+
+export class UploadCarImageController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const images = req.files as IFiles[];
+
+    const uploadCarImageUsecase = container.resolve(UploadCarImageUsecase);
+
+    const images_name = images.map((file) => file.filename);
+
+    await uploadCarImageUsecase.execute({
+      car_id: id,
+      images_name,
+    });
+
+    return res.status(201).send();
+  }
+}
