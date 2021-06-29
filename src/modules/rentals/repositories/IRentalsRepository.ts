8099903e4700c3a2a3bc5ac7b2385bec08cbebd4
Vitor Quadros <viitorquadros@hotmail.com>
2021-06-29 16:23:59 -0300

    feat (dev): finish create method on RentalsRepositoryInMemory

--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -1,6 +1,8 @@
+import { ICreateRentalDTO } from '../DTOs/ICreateRentalDTO';
 import { Rental } from '../infra/typeorm/entities/Rental';
 
 export interface IRentalsRepository {
+  create(data: ICreateRentalDTO): Promise<Rental>;
   findOpenRentalByCar(car_id: string): Promise<Rental>;
   findOpenRentalByUser(user_id: string): Promise<Rental>;
 }
--- src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
+++ src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
@@ -1,7 +1,26 @@
+import { ICreateRentalDTO } from '@modules/rentals/DTOs/ICreateRentalDTO';
 import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
 import { IRentalsRepository } from '../IRentalsRepository';
 
 export class RentalsRepositoryInMemory implements IRentalsRepository {
+  async create({
+    car_id,
+    expected_return_date,
+    user_id,
+  }: ICreateRentalDTO): Promise<Rental> {
+    const rental = new Rental();
+
+    Object.assign(rental, {
+      car_id,
+      expected_return_date,
+      user_id,
+      start_date: new Date(),
+    });
+
+    this.rentals.push(rental);
+    return rental;
+  }
+
   rentals: Rental[] = [];
 
   async findOpenRentalByCar(car_id: string): Promise<Rental> {
