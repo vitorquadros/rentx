4fbb3f4777c0613967a9b57ed2e0dcdc04ad0770
Vitor Quadros <viitorquadros@hotmail.com>
2021-07-01 16:47:48 -0300

    feat: create devolution rental route

--- src/shared/infra/http/routes/rentals.routes.ts
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -1,9 +1,16 @@
 import { CreateRentalController } from '@modules/rentals/usecases/createRental/CreateRentalController';
+import { DevolutionRentalController } from '@modules/rentals/usecases/devolutionRental/DevolutionRentalController';
 import { Router } from 'express';
 import { ensureAuthentication } from '../middlewares/ensureAuthenticated';
 
 export const rentalsRoutes = Router();
 
 const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
 
 rentalsRoutes.post('/', ensureAuthentication, createRentalController.handle);
+rentalsRoutes.post(
+  '/devolution/:id',
+  ensureAuthentication,
+  devolutionRentalController.handle
+);
