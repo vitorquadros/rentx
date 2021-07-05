6ad93670475013dcb66f16b202f9cc9481b7bfb5
Vitor Quadros <viitorquadros@hotmail.com>
2021-07-05 16:37:09 -0300

    feat: create refresh token route

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import { AuthenticateUserController } from '@modules/accounts/usecases/authenticateUser/AuthenticateUserController';
+import { RefreshTokenController } from '@modules/accounts/usecases/refreshToken/RefreshTokenController';
 
 export const authenticateRoutes = Router();
 
 const authenticateUserController = new AuthenticateUserController();
+const refreshTokenController = new RefreshTokenController();
 
 authenticateRoutes.post('/sessions', authenticateUserController.handle);
+authenticateRoutes.post('/refresh-token', refreshTokenController.handle);
