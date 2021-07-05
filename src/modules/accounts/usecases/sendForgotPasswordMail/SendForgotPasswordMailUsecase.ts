db939b86688c162545abd5d6d9a24d4a49f60af7
Vitor Quadros <viitorquadros@hotmail.com>
2021-07-05 18:45:59 -0300

    feat: instance mail provider on SendForgotPasswordMailUsecase

--- src/modules/accounts/usecases/sendForgotPasswordMail/SendForgotPasswordMailUsecase.ts
+++ src/modules/accounts/usecases/sendForgotPasswordMail/SendForgotPasswordMailUsecase.ts
@@ -4,16 +4,19 @@ import { UsersTokensRepository } from '@modules/accounts/infra/typeorm/repositor
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
 import { AppError } from '@shared/errors/AppError';
 import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
+import { IMailProvider } from '@shared/container/providers/MailProvider/IMailProvider';
 
 @injectable()
 export class SendForgotPasswordMailUsecase {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
-    @inject('UsersTokensReopsitory')
+    @inject('UsersTokensRepository')
     private usersTokensRepository: UsersTokensRepository,
     @inject('DayjsDateProvider')
-    private dateProvider: IDateProvider
+    private dateProvider: IDateProvider,
+    @inject('EtherealMailProvider')
+    private mailProvider: IMailProvider
   ) {}
 
   async execute(email: string): Promise<void> {
@@ -32,5 +35,11 @@ export class SendForgotPasswordMailUsecase {
       user_id: user.id,
       expires_date,
     });
+
+    await this.mailProvider.sendMail(
+      email,
+      'Password Recover',
+      `The link to reset your password is ${token}`
+    );
   }
 }
