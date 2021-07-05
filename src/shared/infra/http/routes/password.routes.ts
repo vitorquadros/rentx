bc10d8eded18906d94c370626c81d8d3d7f3984d
Vitor Quadros <viitorquadros@hotmail.com>
2021-07-05 20:14:33 -0300

    feat: create reset password route

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,8 +1,11 @@
+import { ResetPasswordUserController } from '@modules/accounts/usecases/resetPasswordUser/ResetPasswordUserController';
 import { SendForgotPasswordMailController } from '@modules/accounts/usecases/sendForgotPasswordMail/SendForgotPasswordMailController';
 import { Router } from 'express';
 
 export const passwordRoutes = Router();
 
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
+const resetPasswordUserController = new ResetPasswordUserController();
 
 passwordRoutes.post('/forgot', sendForgotPasswordMailController.handle);
+passwordRoutes.post('/reset', resetPasswordUserController.handle);
