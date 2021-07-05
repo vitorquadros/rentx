0dbb88ea1d6e408166a8c16db680ca3b661a2577
Vitor Quadros <viitorquadros@hotmail.com>
2021-07-05 15:22:39 -0300

    feat: create UsersTokensRepository

---
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -0,0 +1,27 @@
+import { getRepository, Repository } from 'typeorm';
+import { ICreateUserTokenDTO } from '@modules/accounts/DTOs/ICreateUserTokenDTO';
+import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokensRepository';
+import { UserTokens } from '../entities/UserTokens';
+
+export class UsersTokensRepository implements IUsersTokensRepository {
+  private repository: Repository<UserTokens>;
+
+  constructor() {
+    this.repository = getRepository(UserTokens);
+  }
+
+  async create({
+    expires_date,
+    refresh_token,
+    user_id,
+  }: ICreateUserTokenDTO): Promise<UserTokens> {
+    const userToken = this.repository.create({
+      expires_date,
+      refresh_token,
+      user_id,
+    });
+
+    await this.repository.save(userToken);
+    return userToken;
+  }
+}
