7b27c3ca7cb6cb9ba25d60eb17c3d1d99672dfb0
Vitor Quadros <viitorquadros@hotmail.com>
2021-07-05 15:46:11 -0300

    feat: create refresh token on AuthenticateUserUsecase

--- src/modules/accounts/usecases/authenticateUser/AuthenticateUserUsecase.ts
+++ src/modules/accounts/usecases/authenticateUser/AuthenticateUserUsecase.ts
@@ -3,6 +3,9 @@ import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepositor
 import { compare } from 'bcrypt';
 import { sign } from 'jsonwebtoken';
 import { AppError } from '@shared/errors/AppError';
+import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokensRepository';
+import auth from '@config/auth';
+import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
 
 interface IRequest {
   email: string;
@@ -15,18 +18,30 @@ interface IResponse {
     email: string;
   };
   token: string;
+  refresh_token: string;
 }
 
 @injectable()
 export class AuthenticateUserUsecase {
   constructor(
     @inject('UsersRepository')
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+    @inject('UsersTokensRepository')
+    private usersTokensRepository: IUsersTokensRepository,
+    @inject('DayjsDateProvider')
+    private dateProvider: IDateProvider
   ) {}
 
   async execute({ email, password }: IRequest): Promise<IResponse> {
     // 1) Verify if user exists
     const user = await this.usersRepository.findByEmail(email);
+    const {
+      jwt_expires_in,
+      jwt_refresh_token_secret,
+      jwt_secret,
+      expires_in_refresh_token,
+      expires_refresh_token_days,
+    } = auth;
 
     if (!user) {
       throw new AppError('Email or password incorrect');
@@ -40,9 +55,25 @@ export class AuthenticateUserUsecase {
     }
 
     // 3) Generate jsonwebtoken
-    const token = sign({}, 'b17a771fab2fe4415bd69ce0256ee132', {
+    const token = sign({}, jwt_secret, {
       subject: user.id,
-      expiresIn: '90d',
+      expiresIn: jwt_expires_in,
+    });
+
+    // 4) Refresh Token
+    const refresh_token = sign({ email }, jwt_refresh_token_secret, {
+      subject: user.id,
+      expiresIn: expires_in_refresh_token,
+    });
+
+    const refresh_token_expires_date = this.dateProvider.addDays(
+      expires_refresh_token_days
+    );
+
+    await this.usersTokensRepository.create({
+      user_id: user.id,
+      refresh_token,
+      expires_date: refresh_token_expires_date,
     });
 
     const tokenReturn: IResponse = {
@@ -51,6 +82,7 @@ export class AuthenticateUserUsecase {
         name: user.name,
         email: user.email,
       },
+      refresh_token,
     };
 
     return tokenReturn;
