c6d9d205a1f6fcb45dc0bcaaf1f6cc58b42acf87
Vitor Quadros <viitorquadros@hotmail.com>
2021-07-06 17:58:02 -0300

    feat: create profile route

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -4,6 +4,7 @@ import { CreateUserController } from '@modules/accounts/usecases/createUser/Crea
 import { UpdateUserAvatarController } from '@modules/accounts/usecases/updateUserAvatar/UpdateUserAvatarController';
 import uploadConfig from '@config/upload';
 import { ensureAuthentication } from '@shared/infra/http/middlewares/ensureAuthenticated';
+import { ProfileUserController } from '@modules/cars/usecases/profileUser/ProfileUserController';
 
 export const usersRoutes = Router();
 
@@ -11,6 +12,7 @@ const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
 const updaterUserAvatarController = new UpdateUserAvatarController();
+const profileUserController = new ProfileUserController();
 
 usersRoutes.post('/', createUserController.handle);
 usersRoutes.patch(
@@ -19,3 +21,5 @@ usersRoutes.patch(
   uploadAvatar.single('avatar'),
   updaterUserAvatarController.handle
 );
+
+usersRoutes.get('/profile', ensureAuthentication, profileUserController.handle);
