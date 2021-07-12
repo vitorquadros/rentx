68982d045d7e2d38534c4f459d9f368e379ebff3
Vitor Quadros <viitorquadros@hotmail.com>
2021-07-12 13:06:44 -0300

    fix: docker database config

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -4,12 +4,11 @@
 
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async (host = 'database'): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: 'localhost',
       database:
         process.env.NODE_ENV === 'test'
           ? 'rentx_test'
